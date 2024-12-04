8840d146aa45189772b466328dad01d5a861e1dd
Lucas Aubry <aubrylucas.pro@gmail.com>
2024-12-04 21:36:38 +0100

    +| bug compil mlx

--- include/cub3d.h
+++ include/cub3d.h
@@ -4,10 +4,23 @@
 /* ========== INCLUDE ============= */
 
 #include "mlx.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "string.h"
+#include "stdlib.h"
 
 /* ========== ENUM ================ */
 
 /* ========== STRUCTURE =========== */
+
+typedef struct s_game
+{
+	int	size_y;
+	int	size_x;
+
+	void	*mlx;
+
+}	t_game;
 
 /* ========== ERROR ================*/
 
